9cf69aa910e9b63d6dbe835580e7a6304fb054fa
Eugene Shved <snuggleshved@gmail.com>
2024-03-22 20:50:30 +0100

    Update config for db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_13_192715) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_002007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_13_192715) do
     t.string "name"
     t.string "mana_cost"
     t.float "cmc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "directors", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.string "genre"
+    t.integer "director_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
